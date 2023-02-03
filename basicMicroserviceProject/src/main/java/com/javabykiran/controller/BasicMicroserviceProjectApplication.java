f7870f4945c6c73d7526292b7d9f7eeed573c8c0
Admin <Admin@DESKTOP-LOTHPN7>
2023-02-03 14:12:02 +0530

    Project Added

---
+++ basicMicroserviceProject/src/main/java/com/javabykiran/controller/BasicMicroserviceProjectApplication.java
@@ -0,0 +1,14 @@
+package com.javabykiran.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicMicroserviceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicMicroserviceProjectApplication.class, args);
+	//System.out.println("Hi");
+	}
+
+}
---
+++ basicMicroserviceProject/src/main/java/com/javabykiran/controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.javabykiran.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+	@RequestMapping("firstservice")
+	public String welcomeMessage() {
+		return
+"We started spring boot at javabykiran";
+	}
+
+}
