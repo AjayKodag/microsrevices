731770b9194360362d7b750225f744748c7e4bf8
Ajay <kodag.ajay@gmail.com>
2023-02-04 19:02:13 +0530

    Upload and commit

---
+++ exercise2-springboot-api/src/main/java/com/jbk/exercise2springbootapi/Exercise2SpringbootApiApplication.java
@@ -0,0 +1,13 @@
+package com.jbk.exercise2springbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise2SpringbootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise2SpringbootApiApplication.class, args);
+	}
+
+}
---
+++ exercise2-springboot-api/src/test/java/com/jbk/exercise2springbootapi/Exercise2SpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbk.exercise2springbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise2SpringbootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
